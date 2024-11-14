b24ae8fcf0b8d1346f2b91f1a8285fe0a98f7b47
FCiraci <furkan.ciraci@icloud.com>
2024-11-14 17:19:49 +0100

    Ajout du retour d'IP et de ping d'adresse IP

--- EduTool.py
+++ EduTool.py
@@ -1,12 +1,13 @@
 from colorama import *
 import os
 import socket
+from ping3 import *
 
 init(autoreset=True)
 
 class EduTool:
-    def __init__(self):
-        self.logo = """ 
+	def __init__(self):
+		self.logo = (Fore.YELLOW + """ 
   _______  ________   ____  ____  ___________  ______      ______    ___       
  /"     "||"      "\\ ("  _||_ " |("     _   ")/    " \\    /    " \\  |"  |      
 (: ______)(.  ___  :)|   (  ) : | )__/  \\\\__/// ____  \\  // ____  \\ ||  |      
@@ -15,38 +16,47 @@ class EduTool:
 (:      "||:       :) /\\\\ __ //\\     \\:  |  \\        /  \\        /  ( \\_|:  \\  
  \\_______)(________/ (__________)     \\__|   \\"_____/    \\"_____/    \\_______) 
 																			  
-"""
-        self.username = os.getlogin()
+""" + Style.RESET_ALL)
+		self.username = os.getlogin()
 
-    def display_intro(self):
-        print(self.logo)
-        print(Fore.GREEN + f"		         Bienvenue {self.username} ! " + Style.RESET_ALL)
-        print(Fore.RED + "		  	 © Copyright Furkan Ciraci" + Style.RESET_ALL)
-        print("⚠ CE N'EST PAS UN TOOL POUR ATTAQUER VOUS NE RECUPEREZ QUE VOS PROPRES INFORMATIONS ⚠")
-        print("Commande :")
-        print("1 : Récuperer les informations systèmes")
-        print("2 : Afficher le nom de l'utilisateur")
-        print("3 : Récuperer votre adresse IP")
+	def display_intro(self):
+		print(self.logo)
+		print(Fore.GREEN + f"		         Bienvenue {self.username} ! " + Style.RESET_ALL)
+		print(Fore.RED + "		  	 © Copyright Furkan Ciraci \n\n\n" + Style.RESET_ALL)
+		print("⚠ CE N'EST PAS UN TOOL POUR ATTAQUER VOUS NE RECUPEREZ QUE VOS PROPRES INFORMATIONS ⚠\n\n")
+		print("Commande :")
+		print("1 : Récuperer les informations systèmes")
+		print("2 : Afficher le nom de l'utilisateur")
+		print("3 : Récuperer votre adresse IP")
+		print("4 : Pinger une adresse IP distante")
 
-    def run(self):
-        while True:
-            try:
-                saisie = int(input("\nEntrez une commande : "))
-                match saisie:
-                    case 1:
-                        print(os.uname())
-                    case 2:
-                        print(self.username)
-                    case 3:
-                        adresse_ip = socket.gethostbyname(self.username)
-                        print(adresse_ip)
-                    case _:
-                        print("Action inconnue, veuillez saisir une valeur présente ci-dessus")
-            except ValueError:
-                print("Veuillez entrer un nombre entier valide.")
+	def run(self):
+		while True:
+			try:
+				saisie = int(input("\nEntrez une commande : "))
+				match saisie:
+					case 1:
+						print(os.uname())
+					case 2:
+						print(self.username)
+					case 3:
+						adresse_ip = socket.gethostbyname(self.username)
+						print(f"Votre adresse IP est : {adresse_ip}")
+					case 4:
+						ip_distant = input("Entrez l'adresse IP à pinger : ")
+						ping_time = ping(ip_distant)
+						if ping_time is None:
+							print(f"Le ping vers {ip_distant} a échoué.")
+						else:
+							print(f"Le ping vers {ip_distant} a réussi avec un temps de {ping_time} secondes.")
+			except ValueError:
+				print("Veuillez entrer un nombre entier valide.")
+			except KeyboardInterrupt:
+				print("\n\nMerci d'avoir utilisé mon tool ! (CTRL-C appuyé)")
+				break
 
 
 if __name__ == "__main__":
-    app = EduTool()
-    app.display_intro()
-    app.run()
+	app = EduTool()
+	app.display_intro()
+	app.run()
