57e26075017d91e25cee435f6e5f856144c3b6b7
FCiraci <furkan.ciraci@icloud.com>
2024-11-19 11:41:34 +0100

    Modifications majeur, changement récuperation IP on récupère maintenent le WLAN, get_ip() et ping_adresse() ont été mis dans des fonctions afin de rendre ma fonction run() plus agréable à lire

--- EduTool.py
+++ EduTool.py
@@ -19,6 +19,13 @@ class EduTool:
 """ + Style.RESET_ALL)
 		self.username = os.getlogin()
 
+	def check_sudo(self):
+		if os.geteuid() != 0:
+			print(Fore.RED + "Ce programme nécessite les privilèges sudo pour fonctionner.")
+			print("Veuillez le relancer avec sudo : `sudo python3 EduTool.py`" + Style.RESET_ALL)
+			exit()
+
+
 	def display_intro(self):
 		print(self.logo)
 		print(Fore.GREEN + f"		         Bienvenue {self.username} ! " + Style.RESET_ALL)
@@ -28,6 +35,28 @@ class EduTool:
 		print("2 : Afficher le nom de l'utilisateur")
 		print("3 : Récuperer votre adresse IP")
 		print("4 : Pinger une adresse IP distante")
+  
+  
+	def get_ip(self):
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		try:
+			s.connect(('10.255.255.255', 1))
+			IP = s.getsockname()[0]
+		except:
+			IP = '127.0.0.1'
+		finally:
+			s.close()
+		print(f"Votre adresse IP est : {IP}")
+
+
+	def ping_adresse(self):
+		ip_distant = input("Entrez l'adresse IP à pinger : ")
+		ping_time = ping(ip_distant)
+		if ping_time is None:
+			print(f"Le ping vers {ip_distant} a échoué.")
+		else:
+			print(f"Le ping vers {ip_distant} a réussi avec un temps de {ping_time} secondes.")
+   
 
 	def run(self):
 		while True:
@@ -39,15 +68,9 @@ class EduTool:
 					case 2:
 						print(self.username)
 					case 3:
-						adresse_ip = socket.gethostbyname(self.username)
-						print(f"Votre adresse IP est : {adresse_ip}")
+						self.get_ip()
 					case 4:
-						ip_distant = input("Entrez l'adresse IP à pinger : ")
-						ping_time = ping(ip_distant)
-						if ping_time is None:
-							print(f"Le ping vers {ip_distant} a échoué.")
-						else:
-							print(f"Le ping vers {ip_distant} a réussi avec un temps de {ping_time} secondes.")
+						self.ping_adresse()
 			except ValueError:
 				print("Veuillez entrer un nombre entier valide.")
 			except KeyboardInterrupt:
@@ -57,5 +80,7 @@ class EduTool:
 
 if __name__ == "__main__":
 	app = EduTool()
+	app.check_sudo()
 	app.display_intro()
 	app.run()
+ 
